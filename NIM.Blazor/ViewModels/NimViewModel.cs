c7597c1fe48a7c3599454a7eb7f682c6144ac624
Claas <claas.moehlmann@outlook.com>
2019-10-28 08:03:43 +0100

    Make it work

--- NIM.Blazor/ViewModels/NimViewModel.cs
+++ NIM.Blazor/ViewModels/NimViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace NIM.Blazor.ViewModels
+﻿namespace NIM.Blazor.ViewModels
 {
     public class NimViewModel
     {
