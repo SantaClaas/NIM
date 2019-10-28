a82c1fd5528f3fd82b80e1c2409d696c0f5d3703
Claas Möhlmann <claas.moehlmann@outlook.com>
2019-10-28 21:54:18 +0100

    add matthias' work

---
+++ NIM/Rules.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace NIM
+{
+    public class Rules
+    {
+        public static Rules Default { get; }
+
+        static Rules()
+        {
+            Default = new Rules(
+                new ReadOnlyCollection<Move>(new[]
+                {
+                    new Move(new []{1,0,0,0}),
+                    new Move(new []{2,0,0,0}),
+                    new Move(new []{3,0,0,0}),
+                    new Move(new []{0,1,0,0}),
+                    new Move(new []{0,2,0,0}),
+                    new Move(new []{0,3,0,0}),
+                    new Move(new []{0,0,1,0}),
+                    new Move(new []{0,0,2,0}),
+                    new Move(new []{0,0,3,0}),
+                    new Move(new []{0,0,0,1}),
+                    new Move(new []{0,0,0,2}),
+                    new Move(new []{0,0,0,3})
+                }),
+                 new Playground(new[] { 1, 2, 3, 4 }),
+                true,
+                2
+                );
+        }
+
+        public ReadOnlyCollection<Move> ValidMoves { get; }
+
+        public Playground StartingField { get; }
+
+        public bool LastMoveWins { get; }
+
+        public int PlayerCount { get; }
+
+        private Rules(ReadOnlyCollection<Move> moves, Playground field, bool? win, int? players)
+        {
+            ValidMoves = moves ?? Default.ValidMoves;
+            StartingField = field ?? Default.StartingField;
+            LastMoveWins = win ?? Default.LastMoveWins;
+            PlayerCount = players ?? Default.PlayerCount;
+        }
+
+        public static Builder Build(IEnumerable<int> rows)
+        {
+            return new Builder(rows);
+        }
+
+        public bool IsMoveValid(Move move, Playground playground)
+        {
+            if (!ValidMoves.Contains(move))
+                return false;
+
+            return !playground.Rows.Where((s, i) => s < move.ChangesPerRow[i]).Any();
+        }
+
+        public List<Move> GetValidMoves(Playground playground)
+        {
+            return ValidMoves.Where(m => IsMoveValid(m, playground)).ToList();
+        }
+
+        public class Builder
+        {
+            private List<Move> _validMoves;
+
+            private readonly Playground _startingField;
+
+            private bool? _lastMoveWins;
+
+            private int? _playerCount;
+
+            internal Builder(IEnumerable<int> rows)
+            {
+                _startingField = new Playground(rows);
+                _validMoves = null;
+                _lastMoveWins = null;
+                _playerCount = null;
+            }
+
+            public Rules Create()
+            {
+                return new Rules(_validMoves is null ? null : new ReadOnlyCollection<Move>(_validMoves.Distinct().ToList()), new Playground(_startingField), _lastMoveWins, _playerCount);
+            }
+
+            public Builder AddRules(params Move[] moves)
+            {
+                if (_validMoves is null)
+                    _validMoves = new List<Move>();
+
+                if (moves.Any(m => m.ChangesPerRow.Count != _startingField.Rows.Count || m.ChangesPerRow.Any(a => a < 0)))
+                    throw new Exception("Invalid move");
+
+                _validMoves.AddRange(moves);
+                return this;
+            }
+
+            public Builder AddSingleRowRules(int min, int max)
+            {
+                for (int row = 0; row < _startingField.Rows.Count; ++row)
+                {
+                    int[] rows = new int[_startingField.Rows.Count];
+                    for (int i = min; i <= max; ++i)
+                    {
+                        rows[row] = i;
+                        AddRules(new Move(rows));
+                    }
+                }
+
+                return this;
+            }
+
+            public Builder AddMultiRowRules(params int[] amountPerRow)
+            {
+                if (amountPerRow.Length > _startingField.Rows.Count)
+                    throw new Exception("Too many rows");
+
+                int[] rows = new int[_startingField.Rows.Count];
+                Array.Copy(amountPerRow, rows, amountPerRow.Length);
+
+                foreach (IEnumerable<int> permutation in Permutations(rows))
+                    AddRules(new Move(permutation));
+
+                return this;
+            }
+
+            public Builder LastMoveWins()
+            {
+                _lastMoveWins = true;
+                return this;
+            }
+
+            public Builder LastMoveLooses()
+            {
+                _lastMoveWins = false;
+                return this;
+            }
+
+            public Builder Players(int count)
+            {
+                _playerCount = count;
+                return this;
+            }
+
+            private IEnumerable<IEnumerable<T>> Permutations<T>(IEnumerable<T> items)
+            {
+                List<T> list = items.ToList();
+                if (list.Count < 2)
+                    yield return list;
+                else
+                {
+                    foreach (T item in list)
+                    {
+                        foreach (IEnumerable<T> permutation in Permutations<T>(list.Where(i => !i.Equals(item))))
+                            yield return permutation.Prepend(item);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NimTests/Tests.cs
@@ -0,0 +1,269 @@
+using System;
+using System.Collections.Generic;
+using NIM;
+using NUnit.Framework;
+
+namespace NimTests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test_Move()
+        {
+            Move moveA = new Move(new[] { 1, 2, 3, 4 });
+
+            Assert.AreEqual(4, moveA.ChangesPerRow.Count);
+            Assert.AreEqual(1, moveA.ChangesPerRow[0]);
+            Assert.AreEqual(2, moveA.ChangesPerRow[1]);
+            Assert.AreEqual(3, moveA.ChangesPerRow[2]);
+            Assert.AreEqual(4, moveA.ChangesPerRow[3]);
+
+
+            Move moveB = new Move(new[] { 1, 2, 3, 4 });
+
+            Assert.True(moveA.Equals(moveB));
+        }
+
+        [Test]
+        public void Test_Playground()
+        {
+            Playground playgroundA = new Playground(new[] { 5, 6 });
+
+            Assert.AreEqual(2, playgroundA.Rows.Count);
+            Assert.AreEqual(5, playgroundA.Rows[0]);
+            Assert.AreEqual(6, playgroundA.Rows[1]);
+
+
+            Playground playgroundB = new Playground(playgroundA);
+
+            Assert.AreEqual(2, playgroundB.Rows.Count);
+            Assert.AreEqual(5, playgroundB.Rows[0]);
+            Assert.AreEqual(6, playgroundB.Rows[1]);
+
+
+            Move move = new Move(new[] { 1, 3 });
+
+            Playground playgroundC = playgroundA.ApplyMove(move);
+
+            Assert.AreEqual(2, playgroundA.Rows.Count);
+            Assert.AreEqual(5, playgroundA.Rows[0]);
+            Assert.AreEqual(6, playgroundA.Rows[1]);
+
+            Assert.AreEqual(2, playgroundB.Rows.Count);
+            Assert.AreEqual(5, playgroundB.Rows[0]);
+            Assert.AreEqual(6, playgroundB.Rows[1]);
+
+            Assert.AreEqual(2, playgroundC.Rows.Count);
+            Assert.AreEqual(4, playgroundC.Rows[0]);
+            Assert.AreEqual(3, playgroundC.Rows[1]);
+        }
+
+        [Test]
+        public void Test_Rules_Builder_Create()
+        {
+            Assert.NotNull(Rules.Default.ValidMoves);
+            Assert.NotNull(Rules.Default.StartingField);
+
+            Rules.Builder builder = Rules.Build(new[] { 10, 15 });
+
+            Rules rules = builder.Create();
+
+            Assert.AreEqual(Rules.Default.LastMoveWins, rules.LastMoveWins);
+            Assert.AreEqual(Rules.Default.PlayerCount, rules.PlayerCount);
+            Assert.AreEqual(Rules.Default.ValidMoves.Count, rules.ValidMoves.Count);
+            for (int i = 0; i < Rules.Default.ValidMoves.Count; ++i)
+                Assert.True(Rules.Default.ValidMoves[i].Equals(rules.ValidMoves[i]));
+            Assert.AreEqual(2, rules.StartingField.Rows.Count);
+            Assert.AreEqual(10, rules.StartingField.Rows[0]);
+            Assert.AreEqual(15, rules.StartingField.Rows[1]);
+
+
+            builder = Rules.Build(new[] { 20, 21 });
+
+            builder.AddRules(new Move(new[] { 7, 9 }));
+
+            Assert.Throws<Exception>(() => builder.AddRules(new Move(new[] { -1, 0 })));
+            Assert.Throws<Exception>(() => builder.AddRules(new Move(new[] { 1, 0, 0 })));
+
+            builder.LastMoveWins();
+
+            builder.Players(3);
+
+            rules = builder.Create();
+
+            Assert.AreEqual(true, rules.LastMoveWins);
+            Assert.AreEqual(3, rules.PlayerCount);
+            Assert.AreEqual(1, rules.ValidMoves.Count);
+            Assert.True(rules.ValidMoves[0].Equals(new Move(new[] { 7, 9 })));
+            Assert.AreEqual(2, rules.StartingField.Rows.Count);
+            Assert.AreEqual(20, rules.StartingField.Rows[0]);
+            Assert.AreEqual(21, rules.StartingField.Rows[1]);
+        }
+
+        [Test]
+        public void Test_Rules_Builder_Moves()
+        {
+            Rules.Builder builder = Rules.Build(new[] { 10, 20 });
+            builder.AddSingleRowRules(1, 3);
+            Rules rules = builder.Create();
+
+            Assert.AreEqual(6, rules.ValidMoves.Count);
+            Assert.True(rules.ValidMoves.Contains(new Move(new[] { 0, 1 })));
+            Assert.True(rules.ValidMoves.Contains(new Move(new[] { 0, 2 })));
+            Assert.True(rules.ValidMoves.Contains(new Move(new[] { 0, 3 })));
+            Assert.True(rules.ValidMoves.Contains(new Move(new[] { 1, 0 })));
+            Assert.True(rules.ValidMoves.Contains(new Move(new[] { 2, 0 })));
+            Assert.True(rules.ValidMoves.Contains(new Move(new[] { 3, 0 })));
+
+
+            builder = Rules.Build(new[] { 30, 40, 50 });
+            builder.AddMultiRowRules(1, 2);
+            rules = builder.Create();
+
+            Assert.AreEqual(6, rules.ValidMoves.Count);
+            Assert.True(rules.ValidMoves.Contains(new Move(new[] { 0, 1, 2 })));
+            Assert.True(rules.ValidMoves.Contains(new Move(new[] { 0, 2, 1 })));
+            Assert.True(rules.ValidMoves.Contains(new Move(new[] { 1, 0, 2 })));
+            Assert.True(rules.ValidMoves.Contains(new Move(new[] { 1, 2, 0 })));
+            Assert.True(rules.ValidMoves.Contains(new Move(new[] { 2, 0, 1 })));
+            Assert.True(rules.ValidMoves.Contains(new Move(new[] { 2, 1, 0 })));
+        }
+
+        [Test]
+        public void Test_Rules_ValidMoves()
+        {
+            Rules rules = Rules.Build(new[] { 1, 2, 3 }).AddSingleRowRules(1, 1).Create();
+
+            Assert.True(rules.IsMoveValid(new Move(new[] { 0, 0, 1 }), new Playground(new[] { 2, 2, 2 })));
+            Assert.False(rules.IsMoveValid(new Move(new[] { 0, 0, 2 }), new Playground(new[] { 2, 2, 2 })));
+            Assert.False(rules.IsMoveValid(new Move(new[] { 0, 0, 1 }), new Playground(new[] { 2, 2, 0 })));
+
+            List<Move> validMoves = rules.GetValidMoves(new Playground(new[] { 0, 1, 2 }));
+
+            Assert.AreEqual(2, validMoves.Count);
+            Assert.True(validMoves.Contains(new Move(new[] { 0, 1, 0 })));
+            Assert.True(validMoves.Contains(new Move(new[] { 0, 0, 1 })));
+        }
+
+        [Test]
+        public void Test_Game_Init()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Game(null, new Player[2]));
+            Assert.Throws<ArgumentNullException>(() => new Game(Rules.Default, null));
+            Assert.Throws<ArgumentException>(() => new Game(Rules.Default, new Player[0]));
+            Assert.Throws<Exception>(() => new Game(Rules.Default, new[] { new AiPlayerRandom("A"), new AiPlayerRandom("A") }));
+
+            Game game = new Game(Rules.Default, new[] { new AiPlayerRandom("A"), new AiPlayerRandom("B") });
+
+            Assert.AreEqual(GameState.ChoosePlayer, game.State);
+        }
+
+        [Test]
+        public void Test_Game_Loop()
+        {
+            Game game = new Game(Rules.Default, new[] { new AiPlayerRandom("A"), new AiPlayerRandom("B") });
+
+            List<Player> players = game.Loop();
+
+            Assert.AreEqual(1, players.Count);
+        }
+
+        [Test]
+        public void Test_Game_FullRun()
+        {
+            Rules rules = Rules.Build(new[] { 1, 2 }).LastMoveWins().Players(2).AddRules(new Move(new[] { 1, 0 }), new Move(new[] { 0, 1 })).Create();
+
+            Game game = new Game(rules, new[] { new AiPlayerFirst("A"), new AiPlayerFirst("B") });
+
+            Assert.AreEqual(GameState.ChoosePlayer, game.State);
+            Assert.AreEqual("B", game.CurrentPlayer.Name);
+            Assert.IsNull(game.LastMove);
+            Assert.AreEqual(1, game.CurrentPlayground.Rows[0]);
+            Assert.AreEqual(2, game.CurrentPlayground.Rows[1]);
+            Assert.True(game.Step());
+
+            Assert.AreEqual(GameState.RequestPlayerResponse, game.State);
+            Assert.AreEqual("A", game.CurrentPlayer.Name);
+            Assert.IsNull(game.LastMove);
+            Assert.AreEqual(1, game.CurrentPlayground.Rows[0]);
+            Assert.AreEqual(2, game.CurrentPlayground.Rows[1]);
+            Assert.True(game.Step());
+
+            Assert.AreEqual(GameState.ApplyMove, game.State);
+            Assert.AreEqual("A", game.CurrentPlayer.Name);
+            Assert.True(game.LastMove.Equals(new Move(new[] { 1, 0 })));
+            Assert.AreEqual(1, game.CurrentPlayground.Rows[0]);
+            Assert.AreEqual(2, game.CurrentPlayground.Rows[1]);
+            Assert.True(game.Step());
+
+            Assert.AreEqual(GameState.CheckConditions, game.State);
+            Assert.AreEqual("A", game.CurrentPlayer.Name);
+            Assert.True(game.LastMove.Equals(new Move(new[] { 1, 0 })));
+            Assert.AreEqual(0, game.CurrentPlayground.Rows[0]);
+            Assert.AreEqual(2, game.CurrentPlayground.Rows[1]);
+            Assert.True(game.Step());
+
+            Assert.AreEqual(GameState.ChoosePlayer, game.State);
+            Assert.AreEqual("A", game.CurrentPlayer.Name);
+            Assert.True(game.LastMove.Equals(new Move(new[] { 1, 0 })));
+            Assert.AreEqual(0, game.CurrentPlayground.Rows[0]);
+            Assert.AreEqual(2, game.CurrentPlayground.Rows[1]);
+            Assert.True(game.Step());
+
+            Assert.AreEqual(GameState.RequestPlayerResponse, game.State);
+            Assert.AreEqual("B", game.CurrentPlayer.Name);
+            Assert.True(game.LastMove.Equals(new Move(new[] { 1, 0 })));
+            Assert.AreEqual(0, game.CurrentPlayground.Rows[0]);
+            Assert.AreEqual(2, game.CurrentPlayground.Rows[1]);
+            Assert.True(game.Step());
+
+            Assert.AreEqual(GameState.ApplyMove, game.State);
+            Assert.AreEqual("B", game.CurrentPlayer.Name);
+            Assert.True(game.LastMove.Equals(new Move(new[] { 0, 1 })));
+            Assert.AreEqual(0, game.CurrentPlayground.Rows[0]);
+            Assert.AreEqual(2, game.CurrentPlayground.Rows[1]);
+            Assert.True(game.Step());
+
+            Assert.AreEqual(GameState.CheckConditions, game.State);
+            Assert.AreEqual("B", game.CurrentPlayer.Name);
+            Assert.True(game.LastMove.Equals(new Move(new[] { 0, 1 })));
+            Assert.AreEqual(0, game.CurrentPlayground.Rows[0]);
+            Assert.AreEqual(1, game.CurrentPlayground.Rows[1]);
+            Assert.True(game.Step());
+
+            Assert.AreEqual(GameState.ChoosePlayer, game.State);
+            Assert.AreEqual("B", game.CurrentPlayer.Name);
+            Assert.True(game.LastMove.Equals(new Move(new[] { 0, 1 })));
+            Assert.AreEqual(0, game.CurrentPlayground.Rows[0]);
+            Assert.AreEqual(1, game.CurrentPlayground.Rows[1]);
+            Assert.True(game.Step());
+
+            Assert.AreEqual(GameState.RequestPlayerResponse, game.State);
+            Assert.AreEqual("A", game.CurrentPlayer.Name);
+            Assert.True(game.LastMove.Equals(new Move(new[] { 0, 1 })));
+            Assert.AreEqual(0, game.CurrentPlayground.Rows[0]);
+            Assert.AreEqual(1, game.CurrentPlayground.Rows[1]);
+            Assert.True(game.Step());
+
+            Assert.AreEqual(GameState.ApplyMove, game.State);
+            Assert.AreEqual("A", game.CurrentPlayer.Name);
+            Assert.True(game.LastMove.Equals(new Move(new[] { 0, 1 })));
+            Assert.AreEqual(0, game.CurrentPlayground.Rows[0]);
+            Assert.AreEqual(1, game.CurrentPlayground.Rows[1]);
+            Assert.True(game.Step());
+
+            Assert.AreEqual(GameState.CheckConditions, game.State);
+            Assert.AreEqual("A", game.CurrentPlayer.Name);
+            Assert.True(game.LastMove.Equals(new Move(new[] { 0, 1 })));
+            Assert.AreEqual(0, game.CurrentPlayground.Rows[0]);
+            Assert.AreEqual(0, game.CurrentPlayground.Rows[1]);
+            Assert.True(game.Step());
+
+            Assert.AreEqual(GameState.GameOver, game.State);
+            List<Player> winningPlayers = game.GetWinningPlayers();
+            Assert.AreEqual(1, winningPlayers.Count);
+            Assert.AreEqual("A", winningPlayers[0].Name);
+            Assert.False(game.Step());
+        }
+    }
+}
