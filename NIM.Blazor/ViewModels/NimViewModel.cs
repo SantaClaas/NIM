afb1d581b498e0a9eba1e13ae0d04911beb25aca
Claas <claas.moehlmann@outlook.com>
2019-10-28 12:04:41 +0100

    Remove unused ui and add next turn button plus current player display

--- NIM.Blazor/ViewModels/NimViewModel.cs
+++ NIM.Blazor/ViewModels/NimViewModel.cs
@@ -2,6 +2,17 @@
 {
     public class NimViewModel
     {
-        public int[] Rows { get; set; } = new int[] { 7,8 };
+        public string[] Players { get; set; } = new string[] { "Player 1", "Player 2" };
+        public string CurrentPlayer => Players[ActivePlayer];
+        public int ActivePlayer { get; set; } = 0;
+        public int[] Rows { get; set; } = new int[] { 7, 8 };
+
+        public void NextPlayer()
+        {
+            if (ActivePlayer < Players.Length)
+                ActivePlayer++;
+            else
+                ActivePlayer = 0;
+        }
     }
 }
