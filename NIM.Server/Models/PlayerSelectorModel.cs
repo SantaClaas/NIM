0aef11b8c4dbd42620bcf7c79f62104917c99e07
Claas Möhlmann <claas.moehlmann@outlook.com>
2019-11-03 20:31:53 +0100

    add player selector component

---
+++ NIM.Server/Models/PlayerSelectorModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NIM.Server.Models
+{
+    public class PlayerSelectorModel
+    {
+        public int Number { get; set; }
+        public bool IsAiPlayer { get; set; }
+        public string Name { get; set; }
+        public Player SelectedAiPlayer { get; set; }
+        public float DifficultyAdvancedAi { get; set; }
+        public List<Player> ValidAiPlayers { get; set; }
+
+    }
+    public class AiPlayerChangeArgs
+    {
+        public ChangeEventArgs ChangeEventArgs { get; set; }
+        public int ModelNumber { get; set; }
+    }
+}
