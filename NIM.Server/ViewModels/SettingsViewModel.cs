4b3ffb8edd5b67e705f6c531eeb02e6dc6316bf0
Claas Möhlmann <claas.moehlmann@outlook.com>
2019-11-09 00:18:13 +0100

    Update css

--- NIM.Server/ViewModels/SettingsViewModel.cs
+++ NIM.Server/ViewModels/SettingsViewModel.cs
@@ -1,15 +1,41 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NIM.Server.ViewModels
 {
     public class SettingsViewModel
     {
-        public uint RowsCount { get; set; }
-        public uint RowsMin { get; set; } = 0;
-        public uint RowsMax { get; set; } = 0;
+        private int rowsCount;
+        public int RowsCount
+        {
+            get => rowsCount;
+            set
+            {
+                if (value < 1)
+                    return;
+                while (value > Rows.Count)
+                {
+                    Rows.Add(Rows.Count + 1);
+                }
+                while (value < Rows.Count)
+                {
+                    Rows.RemoveAt(Rows.Count - 1);
+                }
+                rowsCount = value;
+            }
+        }
+        public int RowsMin { get; set; } = 1;
+        public List<int> Rows { get; set; }
         public int ChangesPerRow { get; set; } = 3;
         public int MaxChangesPerRow { get; set; } = 3;
         public int MinChangesPerRow { get; set; } = 1;
         public bool LastMoveWins { get; set; } = true;
+
+        public SettingsViewModel()
+        {
+            Rows = new List<int>(0);
+            RowsCount = 4;
+        }
     }
 }
