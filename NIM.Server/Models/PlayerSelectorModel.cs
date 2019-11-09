39085c03ee8de9c02cd6d1b7da56370fd7e512d6
Claas Möhlmann <claas.moehlmann@outlook.com>
2019-11-09 22:34:51 +0100

    Improve handling of equal names

--- NIM.Server/Models/PlayerSelectorModel.cs
+++ NIM.Server/Models/PlayerSelectorModel.cs
@@ -8,16 +8,11 @@ namespace NIM.Server.Models
 {
     public class PlayerSelectorModel
     {
-        public int Number { get; set; }
         public bool IsAiPlayer { get; set; }
         public string Name { get; set; }
-        public Player SelectedAiPlayer { get; set; }
         public float DifficultyAdvancedAi { get => difficultyAdvancedAi; set { difficultyAdvancedAi = value >= -1 || value <= 1 ? value : difficultyAdvancedAi; } }
         private float difficultyAdvancedAi;
-    }
-    public class AiPlayerChangeArgs
-    {
-        public ChangeEventArgs ChangeEventArgs { get; set; }
-        public int ModelNumber { get; set; }
+        public bool IsNameValid { get; set; } = true;
+
     }
 }
